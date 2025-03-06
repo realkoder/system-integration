9dda6adf17ae714be49f8643c1f8c814be9e3d98
realkoder <alexanderbtcc@gmail.com>
2025-03-06 22:06:28 +0100

    implemented assingment example with socketio and websocket

---
+++ 01._Assignments/06._Client_and_Server_Client_to_Client/02._Nodejs_SocketIO/app.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+
+// This example is based on Socket.IO own docs: https://socket.io/docs/v4/tutorial/step-3
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("New connection:", io.sockets.sockets.size);
+
+  // event message
+  socket.on("message", async (message) => {
+    console.log(`Received message from the client: ${message}`);
+
+    io.emit("message", message);
+  });
+
+  // connection-count
+  socket.on("connection-count", async (message) => {
+    console.log(`Received message from the client: ${message}`);
+
+    io.emit("connection-count", io.sockets.sockets.size);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", io.sockets.sockets.size);
+  });
+});
+
+server.listen(PORT, console.log("Server listening on PORT", PORT));
