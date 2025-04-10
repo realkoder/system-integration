7faff640be5381c00ac95541185345333745cd82
realkoder <alexanderbtcc@gmail.com>
2025-04-10 21:07:08 +0200

    updated repo with missing stuff from elective

---
+++ 18._CORS/01._Node/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+
+// app.use(cors());
+
+// app.use(cors({
+//     origin: "*",
+//     methods: ["GET"]
+// }))
+
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
+app.get("/timestamp", cors(), (req, res) => {
+    res.send({ data: new Date() });
+});
+
+const PORT = Number(process.env.PORT) || 8080;
+app.listen(PORT, () => console.log("Server is running on port", PORT));
