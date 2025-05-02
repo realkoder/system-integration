e17b26ba04baf94b51bc80da8ec2999f77ff05fb
realkoder <alexanderbtcc@gmail.com>
2025-05-02 15:54:06 +0200

    added register script for hooking to webhook

--- 01._Assignments/12._Webhooks/12b._Integrator/index.js
+++ 01._Assignments/12._Webhooks/12b._Integrator/index.js
@@ -10,4 +10,31 @@ app.post("/webhook", (req, res) => {
   res.status(200).send({ data: "OK" });
 });
 
+app.get("/register-webhook", (req, res) => {
+  const url = "https://webhoob20250501214221-cad2cdb4hncsg6bu.northeurope-01.azurewebsites.net/api/webhooks";
+  const payload = {
+      callbackUrl: "https://integrator.loca.lt/webhook",
+      eventTypes: ["PaymentInitiated", "PaymentCompleted"]
+  };
+  fetch(url, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+  })
+  .then(response => {
+      if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+  })
+  .then(data => {
+      console.log("Success:", data);
+  })
+  .catch(error => {
+      console.error("Error:", error);
+  });
+})
+
 app.listen(PORT, console.log("Server is running at PORT", PORT));
